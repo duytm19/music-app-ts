97224d3b0960df849357a2e20eba98e90b7ed94c
duytm19 <tranminhduy1912@gmail.com>
2025-03-15 14:51:08 +0700

    detail song

---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+// Aplayer
+const aplayer = document.querySelector('#aplayer')
+if(aplayer){
+    let dataSong = aplayer.getAttribute("data-song")
+    dataSong= JSON.parse(dataSong)
+
+    let dataSinger = aplayer.getAttribute("data-singer")
+    dataSinger = JSON.parse(dataSinger)
+
+const ap = new APlayer({
+    container: aplayer,
+    audio: [{
+        name: dataSong.title,
+        artist: dataSinger.fullName,
+        url: dataSong.audio,
+        cover: dataSong.avatar
+    }],
+    autoplay:true,
+    volume: 0.8
+});
+
+
+    const avatar = document.querySelector(".singer-detail .inner-avatar")
+
+    ap.on('play',()=>{
+        avatar.computedStyleMap.animationPlayState = "running"
+    })
+
+    
+    ap.on('pause',()=>{
+        avatar.computedStyleMap.animationPlayState = "paused"
+    })
+}
+// End Aplayer
