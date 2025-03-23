03ec426bb75d7fda05ab19d6eddacc9a6abc9ea5
duytm19 <tranminhduy1912@gmail.com>
2025-03-23 13:45:09 +0700

    fix unicode search

--- dist/helpers/convertToSlug.js
+++ dist/helpers/convertToSlug.js
@@ -1,9 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convert = void 0;
+const unidecode_1 = __importDefault(require("unidecode"));
 const convert = (text) => {
+    const unicodeText = (0, unidecode_1.default)(text.trim());
     let newStr = "";
-    newStr = text.trim().replace(/\s+/g, "-");
+    newStr = unicodeText.replace(/\s+/g, "-");
     return newStr;
 };
 exports.convert = convert;
--- helpers/convertToSlug.ts
+++ helpers/convertToSlug.ts
@@ -1,5 +1,8 @@
+import unicode from 'unidecode'
 export const convert = (text:string):string =>{
+    const unicodeText = unicode(text.trim())
     let newStr: string= ""
-     newStr =text.trim().replace(/\s+/g, "-");
+
+     newStr =unicodeText.replace(/\s+/g, "-");
     return newStr
-}
+}       
